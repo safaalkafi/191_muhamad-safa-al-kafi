cf7904a9545d381eece8a0c4490d5d4b42f06ac8
unknown <safaalkafi246@gmail.com>
2024-10-06 00:23:52 +0700

    Data terbaru

---
+++ Untitled-6.py
@@ -0,0 +1,16 @@
+# Langkah 1: Mengambil input dari pengguna
+angka1 = input("Masukkan angka pertama:")
+angka2 = input("Masukkan angka kedua:")
+angka3 = input("masukkan angka ketiga:")
+
+# Langkah 2: Mengonversi input menjadi integer
+angka1 = int(20)
+angka2 = int(20)
+angka3 = int(20)
+
+
+# Langkah 3: Melakukan penjumlahan
+hasil = angka1 + angka2 + angka3
+
+# Langkah 4: Menampilkan hasil
+print("Hasil penjumlahan dari", angka1, "dan", angka2, "dan", "adalah:", hasil)
